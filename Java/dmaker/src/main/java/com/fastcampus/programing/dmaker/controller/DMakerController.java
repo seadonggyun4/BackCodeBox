9264a2f1943146e52b158294409be949bdc90f16
seadonggyun4 <seadonggyun@gmail.com>
2023-11-09 20:02:23 +0900

    #1, dmaker 프로젝트 개발중....

--- Java/dmaker/src/main/java/com/fastcampus/programing/dmaker/controller/DMakerController.java
+++ Java/dmaker/src/main/java/com/fastcampus/programing/dmaker/controller/DMakerController.java
@@ -1,12 +1,16 @@
 package com.fastcampus.programing.dmaker.controller;
 
 
+import com.fastcampus.programing.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programing.dmaker.service.DMakerService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -15,10 +19,10 @@ import java.util.List;
 
 
 /*
-* DMakerController
-* - 각 HTTP에 대한 정의와 경로 매핑
-*
-* */
+ * DMakerController
+ * - 각 HTTP에 대한 정의와 경로 매핑
+ *
+ * */
 
 @Slf4j // lombok 어노테이션 -> 시스템 log를 찍을수 있게 한다.
 @RestController // 컨트롤러 Bean 등록 ->  어노테이션 Controller + ResponseBody:  RestAPI선언 및 Json형식의 반환값
@@ -26,20 +30,26 @@ import java.util.List;
 public class DMakerController {
     private final DMakerService dMakerService;
 
+
+    // developers 리스트 출력 API ========================================
     @GetMapping("/developers") // -> HTTP GET요청 매핑
-    public List<String> getAllDevelopers(){
+    public List<String> getAllDevelopers() {
         log.info("GET /developers HTTP/1.1");
 
         return Arrays.asList("snow", "elsa", "oulaf");
     }
 
+    // developers 생성 API =============================================
+    @PostMapping("/create-developers") // -> HTTP GET요청 매핑
+    public CreateDeveloper.Response createDevelopers(
+            @Valid // -> @Valid어노테이션을 통해 CreatteDeveloper내무에 선언한 @Min, @Max 와 같은 validation 어노테이션이 동작한다.
+            @RequestBody // -> HTTP스팩의 json을 담는다.
+            CreateDeveloper.Request request
+            ) {
+        log.info("request: {}", request);
 
-    @GetMapping("/create-developers") // -> HTTP GET요청 매핑
-    public List<String> createDevelopers(){
-        log.info("GET /create-developers HTTP/1.1");
+        dMakerService.createDeveloper(request);
 
-        dMakerService.createDeveloper();
-
-        return Collections.singletonList("Olaf");
+        return dMakerService.createDeveloper(request);
     }
 }
--- Java/dmaker/src/main/java/com/fastcampus/programing/dmaker/service/DMakerService.java
+++ Java/dmaker/src/main/java/com/fastcampus/programing/dmaker/service/DMakerService.java
@@ -1,13 +1,22 @@
 package com.fastcampus.programing.dmaker.service;
 
 
+import com.fastcampus.programing.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programing.dmaker.entity.Developer;
+import com.fastcampus.programing.dmaker.exception.DMakerErrorCode;
+import com.fastcampus.programing.dmaker.exception.DMakerException;
 import com.fastcampus.programing.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programing.dmaker.type.DeveloperLevel;
 import com.fastcampus.programing.dmaker.type.DeveloperSkillType;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.fastcampus.programing.dmaker.exception.DMakerErrorCode.*;
 
 
 /*
@@ -18,6 +27,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DMakerService {
     private final DeveloperRepository developerRepository;
+    private final EntityManager em;
+
+
 
 
     // ACID
@@ -25,16 +37,89 @@ public class DMakerService {
     // Consistency
     // Isolation
     // Durability
-    @Transactional
-    public void createDeveloper(){
+    @Transactional // -> Transactional 한 코드에서 반복되고있는 패턴을, 어노테이션 기반 포인트컷으로 인해 반복되는 코드인 advice를 자동으로 넣어준다 : AOP
+    public CreateDeveloper.Response createDeveloper(CreateDeveloper.Request request){
+        /*
+        * 여러 작업을 진행하다가 문제가 생겼을 경우 이전 상태로 롤백하기 위해 사용되는 것이 트랜잭션(Transaction) 이다.
+        * 트랜잭션은 더 이상 쪼갤 수 없는 최소 작업 단위를 의미한다. 그래서 트랜잭션은 commit으로 성공 하거나 rollback으로 실패 이후 취소되어야 한다.
+        * 하지만 모든 트랜잭션이 동일한 것은 아니고 속성에 따라 동작 방식을 다르게 해줄 수 있다.
+        *
+        *
+        * => 동시에 진행되고 성공해야 하는 작업중 일부가 실패했을때 다시 전체적으로 그 전 작업으로 롤백하기 위함
+        * */
+
+
+        // ==================== @Transactional 어노테이션 X ====================
+//        EntityTransaction transction = em.getTransaction();
+//        try{
+//            transction.begin();
+//            Developer developer = Developer.builder()
+//                    .developerLevel(DeveloperLevel.JUNGNIOR)
+//                    .developerSkillType(DeveloperSkillType.FRONT_END)
+//                    .experienceYears(2)
+//                    .name("Olaf")
+//                    .age(5)
+//                    .build();
+//
+//            developerRepository.save(developer);
+//            transction.commit();
+//        } catch (Exception e) {
+//            transction.rollback();
+//            throw e;
+//        }
+
+        // ==================== @Transactional 어노테이션 O ====================
+        validateCreateDeveloperRequest(request);
+
+
         Developer developer = Developer.builder()
-                .developerLevel(DeveloperLevel.JUNGNIOR)
-                .developerSkillType(DeveloperSkillType.FRONT_END)
-                .experienceYears(2)
-                .name("Olaf")
-                .age(5)
+                .developerLevel(request.getDeveloperLevel())
+                .developerSkillType(request.getDeveloperSkillType())
+                .experienceYears(request.getExperienceYears())
+                .memberId(request.getMemberId())
+                .name(request.getName())
+                .age(request.getAge())
                 .build();
 
-        developerRepository.save(developer);
+        developerRepository.save(developer);   // db 저장
+        return CreateDeveloper.Response.fromEntity(developer); // 리턴값 반환;
+    }
+
+
+    // ============================================================ request business validation ============================================================
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request){
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+
+        // 시니어 검사
+        if(developerLevel == DeveloperLevel.SENIOR && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        // 중니어 검사
+        if(developerLevel == DeveloperLevel.JUNGNIOR
+                && (experienceYears < 4 || experienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        // 주니어 검사
+        if(developerLevel == DeveloperLevel.JUNIOR && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        // 멤버 아이디 존재여부 검사
+        /*
+        * Optional을 사용하면 예상치 못한 NullPointerException 예외를 제공되는 메소드로 간단히 회피할 수 있다.
+        * 즉, 복잡한 조건문 없이도 널(null) 값으로 인해 발생하는 예외를 처리할 수 있게 된다.
+        *
+        * - isPresent() 메소드 : Optional 객체가 값을 가지고 있다면 true, 값이 없다면 false 리턴
+        * */
+//        Optional<Developer> developer = developerRepository.findByMemberId(request.getMemberId());
+//        if(developer.isPresent()){
+//            throw new DMakerException(DUPLICATED_MEMBER_ID);
+//        }
+        developerRepository.findByMemberId(request.getMemberId()).ifPresent(developer -> {
+            throw new DMakerException(DUPLICATED_MEMBER_ID);
+        });
     }
 }
